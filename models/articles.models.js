f7e7a59b8d4eeea43c501080b8939df5cbb3bdbf
Lentio Sechou <lentiosechou@gmail.com>
2024-05-09 01:32:11 +0100

    updated articles.article_id

--- models/articles.models.js
+++ models/articles.models.js
@@ -9,7 +9,7 @@ function fetchArticle(article_id) {
 
 function fetchAllArticles(topic) {
     
-  let sqlQuery = `SELECT articles.article_id articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comment_id) AS comment_count
+  let sqlQuery = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comment_id) AS comment_count
                       FROM articles 
                       LEFT JOIN comments ON articles.article_id = comments.article_id`;
                       
